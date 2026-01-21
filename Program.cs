002b805009eb22386a1e5189f48eb4174cc3af15
Bao Khanh Tran <ktbv.22@outlook.com>
2026-01-21 10:35:56 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.IO;
+namespace Wk2Assignment2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter basic details to save to file: ");
+            
+        }
+    }
+}
